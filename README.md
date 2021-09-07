15ef3185b9adf93358a615de885d2c0cd6130c18
ZhnMa <78738662+ZhnMa@users.noreply.github.com>
2021-09-08 01:26:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Temp
+ individual repository
---
+++ main.c
@@ -0,0 +1,9 @@
+/*
+ * main.c
+ *
+ *  Created on: 2021Äê9ÔÂ8ÈÕ
+ *      Author: Zhaonan Ma
+ *      For individual project - temperature sensor
+ */
+
+
